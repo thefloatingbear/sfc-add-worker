7fd50511cfc5f0afd62a30e1f9ff1116c343ffe2
Charlie Stewart <charles.stewart@outlook.com>
2018-06-05 10:21:13 +0100

    Changed service name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'NMDS',
+  serviceName: 'Add a worker',
 
   // Default port that prototype runs on
   port: '3000',
